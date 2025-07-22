b1326a97e0bdd05c4f6fe7151d79118d79f53aef
priyankarajesh1403 <priyanka.rajesh1403@gmail.com>
2025-07-22 17:51:31 +0530

    Create contactForm.js

---
+++ force-app/main/default/lwc/contactForm/contactForm.js
@@ -0,0 +1,24 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ContactComponent extends LightningElement {
+    @api data;
+
+    handleChange(event) {
+        const field = event.target.dataset.id;
+        const value = event.target.value;
+
+        this.dispatchEvent(new CustomEvent('childchange', {
+            detail: {
+                section: 'contact',
+                field,
+                value
+            },
+            bubbles: true,
+            composed: true
+        }));
+    }
+
+    @api updateData(newData) {
+        this.data = JSON.parse(JSON.stringify(newData));
+    }
+}
